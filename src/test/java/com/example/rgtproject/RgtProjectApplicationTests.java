d0b06020bd5fb571287aa754f862a61034c18db2
will kim <78planet@gmail.com>
2023-08-09 16:24:43 +0900

    init project

---
+++ src/test/java/com/example/rgtproject/RgtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rgtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgtProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
